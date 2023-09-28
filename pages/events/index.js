e86b9d909ca129958dfcfc79649bc127563da672
Mahmoud Omar <m.ahmedhussienomar@gmail.com>
2023-09-28 18:42:45 +0200

    Feat : Events & Event Filter Component

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,8 +1,15 @@
-export default function AllEventsPage()
-{
+import { Fragment } from "react";
+import EventList from "@/components/events/event-list";
+import EventsSearch from "@/components/events/events-search";
+import { getAllEvents } from "@/dummy-data";
+
+
+export default function AllEventsPage() {
+    const events = getAllEvents();
     return (
-        <div>
-            <h1>All Events</h1>
-        </div>
+        <Fragment>
+            <EventsSearch />
+            <EventList items={events}/>
+        </Fragment>
     );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,20 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import { getFeaturedEvents } from "../dummy-data";
+const inter = Inter({ subsets: ["latin"] });
+import EventList from "@/components/events/event-list";
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function HomePage() {
+
+  const featuredEvents = getFeaturedEvents();
   return (
     <div>
-        <h1>All Events</h1>
+      <EventList items={featuredEvents}/>
     </div>
-   );
+  );
+
+
 }
